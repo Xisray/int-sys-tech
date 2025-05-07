0aacff57763bd3037941dd30e3fac41523a48ac3
Xisray <safixxkir@yandex.ru>
2025-05-07 21:11:15 +0500

    [feat] pz5 done

--- pz5/src/App.jsx
+++ pz5/src/App.jsx
@@ -3,48 +3,125 @@ import "./App.css";
 import { GenerateWeightMatrix, GetTrainingData } from "./utils/Utils";
 import Settings from "./core/Settings";
 import TrainingDataFunctions from "./utils/TrainingDataFunctions";
-import ActivationFunctions from "./core/ActivationFunctions"
+import ActivationFunctions from "./core/ActivationFunctions";
 
-const hz = (trainingVector, inputWeightMatrix, outputWeightMatrix, k) => {
-  const vectorS = new Array(inputWeightMatrix.length).fill(0);
-  for(let i = 0; i < vectorS.length; i++) {
-    for(let j = 0; j < vectorS.length; j++) {
-      vectorS[i] += trainingVector[j]*inputWeightMatrix[j][i];
+function getVectorS(vector, weightMatrix) {
+  const vectorS = new Array(weightMatrix[0].length).fill(0);
+  for (let i = 0; i < vectorS.length; i++) {
+    const actions = [];
+    for (let j = 0; j < weightMatrix.length; j++) {
+      vectorS[i] += vector[j] * weightMatrix[j][i];
+      actions.push(`${vector[j]} * ${weightMatrix[j][i]}`);
     }
+    console.log(`S${i + 1} = ${actions.join(" + ")} = ${vectorS[i]}`);
   }
-  const vectorY = Array.from({length: vectorS.length}).map((_, index) => ActivationFunctions.Sigmoid.func(vectorS[index], k));
-  console.log(vectorY);
+  return vectorS;
+}
+
+function getVectorY(vector, weightMatrix, k) {
+  const vectorS = getVectorS(vector, weightMatrix);
+  return Array.from({ length: vectorS.length }).map((_, index) => ActivationFunctions.Sigmoid.func(vectorS[index], k));
+}
+
+function forwardStep(vectorX, inputWeights, outputWeights, k) {
+  console.log("Прямой ход");
+  console.log(`k = ${k}`);
+  console.log(`Вектор S для входного слоя: `);
+  const vectorY = getVectorY(vectorX, inputWeights, k);
+  console.log(`Вектор Y: [ ${vectorY.join(", ")} ]`);
+
+  console.log(`Вектор S для выходного слоя: `);
+  const vectorSForY = getVectorS(vectorY, outputWeights);
+
+  const y = ActivationFunctions.Sigmoid.func(
+    vectorSForY.reduce((prev, cur) => prev * cur),
+    k
+  );
+  // const y = vectorSForY.reduce((prev, cur) => prev * cur) * speedRatio;
+  console.log(`Y = ${y}`);
+  return [vectorY, y];
+}
+
+function backwardStep(vectorX, d, inputWeights, outputWeights, k, speedRatio) {
+  const [vectorY, y] = forwardStep(vectorX, inputWeights, outputWeights, k);
+
+  console.log("Обратный ход");
+  const deltaSqr = (d - y) * y * (1 - y);
+  console.log(`η = ${speedRatio}`);
+  console.log(`δ² = (${d} - ${y}) * ${y} * (1 - ${y}) = ${deltaSqr}`);
+
+  const newOutputWeights = outputWeights.map((row, rowIndex) =>
+    row.map((cell) => {
+      const result = cell + speedRatio * deltaSqr * vectorY[rowIndex];
+      console.log(`w${rowIndex + 1} = ${cell} + ${speedRatio} * ${deltaSqr} * ${vectorY[rowIndex]} = ${result}`);
+      return result;
+    })
+  );
+  const vectorDelta = vectorY.map((y, index) => {
+    const result = y * (1 - y) * deltaSqr * outputWeights[index][0];
+    console.log(`δ${index + 1} = ${y} * (1 - ${y}) * ${deltaSqr} * ${outputWeights[index][0]} = ${result}`);
+    return result;
+  });
+  const newInputWeights = inputWeights.map((row, rowIndex) =>
+    row.map((cell, colIndex) => {
+      const result = cell + speedRatio * vectorDelta[colIndex] * vectorX[rowIndex];
+      console.log(`w${rowIndex + 1}${colIndex + 1} = ${cell} + ${speedRatio} * ${vectorDelta[colIndex]} * ${vectorX[rowIndex]} = ${result}`);
+      return result;
+    })
+  );
+  const e = getEpsilon(d, y);
+  return {
+    inputWeights: newInputWeights,
+    outputWeights: newOutputWeights,
+    e: e,
+  };
+}
+
+function getEpsilon(expected, fact) {
+  let e = 0;
+  for (let i = 0; i < expected.length; i++) {
+    e += Math.pow(expected[i] - fact[i], 2);
+  }
+  return e;
 }
 
 function App() {
   const [trainingDataFunction, setTrainingDataFunction] = useState("Implication");
-  const [trainingData, setTrainingData] = useState(GetTrainingData(Settings.countInputNeurons, Settings.countOutputNeurons, TrainingDataFunctions.Implication));
-  const [inputWeightMatrix, setInputWeightMatrix] = useState(GenerateWeightMatrix(Settings.countInputNeurons, Settings.countInputNeurons));
-  const [outputWeightMatrix, setOutputWeightMatrix] = useState(GenerateWeightMatrix(Settings.countOutputNeurons, Settings.countInputNeurons));
+  // const [trainingData, setTrainingData] = useState(GetTrainingData(Settings.countInputNeurons, Settings.countOutputNeurons, TrainingDataFunctions.Implication));
+  // const [inputWeightMatrix, setInputWeightMatrix] = useState(GenerateWeightMatrix(Settings.countInputNeurons, Settings.countInputNeurons));
+  // const [outputWeightMatrix, setOutputWeightMatrix] = useState(GenerateWeightMatrix(Settings.countOutputNeurons, Settings.countInputNeurons));
 
-  useEffect(() => {
-    setTrainingData(GetTrainingData(Settings.countInputNeurons, Settings.countOutputNeurons, TrainingDataFunctions[trainingDataFunction]))
-  }, [trainingDataFunction]);
+  const [trainingData, setTrainingData] = useState(Settings.preset.trainingData);
+  const [inputWeightMatrix, setInputWeightMatrix] = useState(Settings.preset.inputWeights);
+  const [outputWeightMatrix, setOutputWeightMatrix] = useState(Settings.preset.outputWeights);
+
+  const test = () => {
+    // forwardStep(trainingData[2], inputWeightMatrix, outputWeightMatrix, Settings.preset.k, Settings.preset.speedRatio);
+    backwardStep(trainingData.matrixX[2], trainingData.vectorD[2], inputWeightMatrix, outputWeightMatrix, Settings.preset.k, Settings.preset.speedRatio);
+  };
+
+  // useEffect(() => {
+  //   setTrainingData(GetTrainingData(Settings.countInputNeurons, Settings.countOutputNeurons, TrainingDataFunctions[trainingDataFunction]))
+  // }, [trainingDataFunction]);
 
   const regenerateWeights = () => {
     setInputWeightMatrix(GenerateWeightMatrix(Settings.countInputNeurons, Settings.countInputNeurons));
     setOutputWeightMatrix(GenerateWeightMatrix(Settings.countOutputNeurons, Settings.countInputNeurons));
-
-  }
+  };
 
   return (
     <div>
-      <button onClick={() => hz([0,1,1], [[0.6, 0.9],[0.1,0.5]], [[0.3, 0.8]], 0.9)}>Тест</button>
+      <button onClick={test}>Тест</button>
       {/* <button onClick={() => hz(trainingData[1], inputWeightMatrix, outputWeightMatrix, Settings.k)}>Тест</button> */}
       <label>
         Функция
-        <select value={trainingDataFunction} onChange={e => setTrainingDataFunction(e.target.value)} name="training-function" id="training-function">
-          {Object.keys(TrainingDataFunctions).map((key => (
+        <select value={trainingDataFunction} onChange={(e) => setTrainingDataFunction(e.target.value)} name="training-function" id="training-function">
+          {Object.keys(TrainingDataFunctions).map((key) => (
             <option value={key}>{key}</option>
-          )))}
+          ))}
         </select>
       </label>
-      <table className="table">
+      {/* <table className="table">
         <thead>
           <tr>
             {Array.from({ length: Settings.countInputNeurons }).map((_, index) => (
@@ -62,8 +139,7 @@ function App() {
             </tr>
           ))}
         </tbody>
-      </table>
-
+      </table> */}
 
       <button onClick={regenerateWeights}>Перегенерировать веса</button>
 
